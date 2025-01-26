3ae060696241b562cbbe8299fe20c7e2e4288a4b
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-01-27 02:52:37 +0300

    Add project files.

---
+++ ApiBooks.WebUI/ViewComponents/_PopularBooksGetByIdComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using ApiBooks.WebUI.Areas.Admin.Dto.BookDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiBooks.WebUI.ViewComponents
+{
+    public class _PopularBooksGetByIdComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _PopularBooksGetByIdComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7051/api/Book/GetBooksByCategoryId?id="+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
