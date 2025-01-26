85da5f1a96abdf191915163d89d8e7f4071f234a
Hüseyin Kurban <huseyinkurban03@gmail.com>
2025-01-27 02:20:44 +0300

    Add project files.

--- ApiBooks.WebUI/ViewComponents/_PopularBooksAllComponentPartial.cs
+++ ApiBooks.WebUI/ViewComponents/_PopularBooksAllComponentPartial.cs
@@ -1,39 +1,30 @@
-﻿using ApiBooks.WebUI.Areas.Admin.Dto.CategoryDto;
+﻿using ApiBooks.WebUI.Areas.Admin.Dto.BookDto;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace ApiBooks.WebUI.ViewComponents
 {
-    public class _PopularBooksComponentPartial:ViewComponent
+    public class _PopularBooksAllComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public _PopularBooksComponentPartial(IHttpClientFactory httpClientFactory)
+        public _PopularBooksAllComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-      
             var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7051/api/Book");
 
-        
-            var responseMessage = await client.GetAsync("https://localhost:7051/api/Category");
-
-       
             if (responseMessage.IsSuccessStatusCode)
             {
-           
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
 
-           
-                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
-
+                var values = JsonConvert.DeserializeObject<List<ResultBookDto>>(jsonData);
                 return View(values);
             }
-
-       
             return View();
         }
     }
